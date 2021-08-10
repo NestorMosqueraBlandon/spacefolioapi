b00dc4224fd4f06276d7153f884f41aad9aee649
Space Polux <spacepoluxmanager@gmail.com>
2021-08-10 00:44:12 -0500

    Transaction

--- backend/utils/resolvers.js
+++ backend/utils/resolvers.js
@@ -1,22 +1,17 @@
 import User from '../models/userModel.js'
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-<<<<<<< HEAD
-=======
-import CoinGecko from 'coingecko-api';
->>>>>>> develop
 
+import CoinGecko from 'coingecko-api';
 const CoinGeckoClient = new CoinGecko();
 
 import { sendConfirmationEmail, sendResetPassword } from '../services/emailService.js';
 import config from './config.js';
-<<<<<<< HEAD
-=======
+
 import BuyManualTransaction from '../models/buyManualTransaction.js';
 import SellManualTransaction from '../models/sellManualTransaction.js';
 import TransferTransaction from '../models/transferTransaction.js';
 
->>>>>>> develop
 
 export const resolvers = {
     Query: {
